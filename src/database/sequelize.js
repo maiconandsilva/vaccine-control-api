325c1e64ea7d59cb3bbb325a3dd58b62e812dbfa
Maicon Mauricio <64911189+maiconandsilva@users.noreply.github.com>
2021-09-09 15:12:02 -0300

    Update database connection settings

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -5,10 +5,13 @@ const env = process.env;
 
 const sequelize = new Sequelize(env.DB_DATABASE, env.DB_USER, env.DB_PASS, {
     host: env.DB_HOST,
+    port: env.DB_PORT,
     dialect: "postgres" /* one of "mysql" | "mariadb" | "postgres" | "mssql" */,
     define: {
-        freezeTableName: true
+        underscored: true,
     },
+    logQueryParameters: env.NODE_ENV === "development",
+    benchmark: env.NODE_ENV === "development",
 });
 
 module.exports = sequelize;
